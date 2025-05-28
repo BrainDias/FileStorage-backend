ffb7f486c1139bae1a93d96a7ccfe71dcc73bdd1
Danyl Paniutchev <yankee.democratic@gmail.com>
2025-05-28 03:17:24 +0300

    feat: added SecurityConfig to reserve access to files for authenticated users only (http basic auth)

---
+++ src/main/java/org/file_storage/configs/SecurityConfig.java
@@ -0,0 +1,43 @@
+package org.file_storage.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user1 = User.withUsername("alice")
+                .password("{noop}password1")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("bob")
+                .password("{noop}password2")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
+
